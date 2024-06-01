37f2aa8553d6b3e4c2820a9d1e281cb014eb2648
Jian165 <118362738+Jian165@users.noreply.github.com>
2024-06-01 09:58:51 +0800

    06/24

--- app/src/main/java/com/markcode/flashshareapp/HomeActivity.java
+++ app/src/main/java/com/markcode/flashshareapp/HomeActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -25,18 +27,40 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         viewPager2 =  findViewById(R.id.ViewPager);
+        viewPager2.setAdapter(new ViewPagerAdopter(this));
         tabLayout = findViewById(R.id.TabLayout);
 
         tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
             public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
 
+                if(position == 0)
+                {
+                    tab.setIcon(R.drawable.icc_dashboard);
+                }
+                else if(position == 1)
+                {
+                    tab.setIcon(R.drawable.icc_flashcard);
+                }
+                else if(position == 2)
+                {
+                    tab.setIcon(R.drawable.icc_profile);
+                }
             }
         });
 
-
-
-
-
+        tabLayoutMediator.attach();
     }
+
+    private void BadgeDrawable(TabLayout.Tab tab)
+    {
+        BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+        badgeDrawable.setBackgroundColor(
+                ContextCompat.getColor(getApplicationContext(),R.color.electric_blue)
+        );
+        badgeDrawable.setVisible(true);
+        badgeDrawable.setNumber(0);
+        badgeDrawable.setMaxCharacterCount(4);
+    }
+
 }
